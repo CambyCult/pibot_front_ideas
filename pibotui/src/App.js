a069ca3d9d94210fe5847248413ce19d6047b15a
Camby <cambycult@gmail.com>
2023-03-09 09:43:19 -0800

    Changes to /field.

--- pibotui/src/App.js
+++ pibotui/src/App.js
@@ -1,10 +1,26 @@
 import Register from './Register';
+import Field from './Field';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
 
   return (
     <main className="App">
-      <Register />
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Register />} />
+            <Route path="/field" element={<Field />} />
+
+          </Routes>
+        </BrowserRouter>
+        
+        <div className="header">
+          <button class="button-1" onclick="openNav()">SB</button>
+          <div class="button-2"></div>
+          <div class="button-3"></div>
+          <div class="button-4"></div>
+        </div>
+      
     </main>
 
   )

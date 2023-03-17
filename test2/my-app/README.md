904a869ebe2e1c47df32fb3abcdd30e9d5c11834
Camby <cambycult@gmail.com>
2023-03-17 08:27:17 -0700

    updated readme with tabulator instructions

---
+++ test2/my-app/README.md
@@ -0,0 +1,21 @@
+
+
+
+
+<!-- Tabulator Installation:
+
+install// npm install react-tabulator
+
+css// 
+import 'react-tabulator/css/tabulator.css';
+import {ReactTabulator} from 'react-tabulator';
+
+component//
+<ReactTabulator
+    data={data}
+    columns={columns}
+    layout={"fitdata"}
+    />
+
+
+ -->
---
+++ test2/my-app/src/App.js
@@ -0,0 +1,43 @@
+import 'react-tabulator/css/tabulator.css';
+import {ReactTabulator} from 'react-tabulator';
+import './App.css';
+
+function App() {
+
+  const columns = [
+        { title: "Item", field: "item", width: 300, responsive:0 },
+        { title: "Size", field: "size", widthGrow: 1, responsive:0 },
+        { title: "Stock", field: "stock", widthGrow: 1, responsive: 0},
+        { title: "reqStock", field: "reqStock", widthGrow: 1, responsive:1 },
+  ];
+  var data = [
+        { id: 1, item: "Nasal Cannula", size: "Adult", stock: 1, reqStock: 4 },
+        { id: 2, item: "Nasal Cannula", size: "Pedi", stock: 3, reqStock: 4 },
+        { id: 3, item: "Bandaids", size: 4, stock: 7, reqStock: 12 },
+        { id: 4, item: "Whole-Blood", size: "O-", stock: 3, reqStock: 9 },
+  ];
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        
+        <div className="container">
+        <ReactTabulator
+            maxheight={"100%"}
+            maxwidth={"75%"}
+            data={data}
+            columns={columns}
+            layout={"fitDataFill"}
+            layoutColumnsOnNewData={"true"}
+            responsiveLayout={"collapse"}
+            textDirection={"rtl"}
+            
+        />
+        </div>
+        <div className="bottom-Bar">test</div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
